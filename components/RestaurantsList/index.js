7d0bf1a36958d321e764928cff4a2987bb6e179a
takumi <takumim1005@gmail.com>
2022-12-21 23:14:52 +0900

    カードリストをmap関数で展開

--- components/RestaurantsList/index.js
+++ components/RestaurantsList/index.js
@@ -1,7 +1,7 @@
+import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import Link from "next/link";
 import { Card, CardBody, CardImg, CardTitle, Col, Row } from "reactstrap";
-import { useQuery } from "@apollo/react-hooks";
 
 const query = gql`
 	{
@@ -18,44 +18,55 @@ const query = gql`
 
 const RestaurantList = () => {
 	const { loading, error, data } = useQuery(query);
-	console.log(data);
-	return (
-		<Row>
-			<Col xs="6" sm="4">
-				<Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-					<CardImg
-						src="http://localhost:1337/uploads/restaurant1_309084d9be.jpg"
-						top={true}
-						style={{ height: 250 }}
-					/>
-					<CardBody>
-						<CardTitle>Italian restaurant</CardTitle>
-						<CardTitle>イタリアンのレストランです。</CardTitle>
-					</CardBody>
-					<div className="card-footer">
-						<Link href="/restaurant?id=1" as="/restaurant/1">
-							<a className="btn btn-primary">もっと見る</a>
-						</Link>
-					</div>
-				</Card>
-			</Col>
-			<style jsx>{`
-				a {
-					color: white;
-				}
-				a:link {
-					text-decoration: none;
-					color: white:
-				}
-				a:hover {
-					color: white;
-				}
-				.card-colums {
-					column-count: 3;
-				}
-			`}</style>
-		</Row>
-	);
+	if (loading) return <h2>ロード中・・・</h2>;
+	if (data.restaurants && data.restaurants.length) {
+		return (
+			<Row>
+				{data.restaurants.map((res) => (
+					<Col xs="6" sm="4" key={res.id}>
+						<Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+							<CardImg
+								src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
+								top={true}
+								style={{ height: 250 }}
+							/>
+							<CardBody>
+								<CardTitle>{res.name}</CardTitle>
+								<CardTitle>{res.description}</CardTitle>
+							</CardBody>
+							<div className="card-footer">
+								<Link
+									href={`/restaurants/${res.id}`}
+									as={`/restaurants?id=${res.id}`}
+								>
+									<a className="btn btn-primary">
+										もっと見る
+									</a>
+								</Link>
+							</div>
+						</Card>
+					</Col>
+				))}
+				<style jsx>{`
+					a {
+						color: white;
+					}
+					a:link {
+						text-decoration: none;
+						color: white:
+					}
+					a:hover {
+						color: white;
+					}
+					.card-colums {
+						column-count: 3;
+					}
+				`}</style>
+			</Row>
+		);
+	} else {
+		return <h1>レストランが見つかりませんでした。</h1>;
+	}
 };
 
 export default RestaurantList;
