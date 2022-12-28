0ddea7666282724b6a10c1cbcffd650e4ef6f5a3
takumi <takumim1005@gmail.com>
2022-12-28 23:42:42 +0900

    link属性修正

--- components/RestaurantsList/index.js
+++ components/RestaurantsList/index.js
@@ -40,8 +40,8 @@ const RestaurantList = (props) => {
 							</CardBody>
 							<div className="card-footer">
 								<Link
-									href={`/restaurants/${res.id}`}
-									as={`/restaurants?id=${res.id}`}
+									as={`/restaurants/${res.id}`}
+									href={`/restaurants?id=${res.id}`}
 								>
 									<a className="btn btn-primary">
 										もっと見る
