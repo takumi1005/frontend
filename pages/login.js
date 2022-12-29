bd54e2fe7270b360fd52ca9ff2571a025e871553
takumi <takumim1005@gmail.com>
2022-12-29 11:17:23 +0900

    ログインコンポーネント作成

---
+++ pages/login.js
@@ -0,0 +1,93 @@
+import { useContext, useState } from "react";
+import {
+	Button,
+	Col,
+	Container,
+	Form,
+	FormGroup,
+	Input,
+	Label,
+	Row,
+} from "reactstrap";
+import AppContext from "../context/AppContext";
+import { registerUser } from "../lib/auth";
+
+const Login = () => {
+	const appContext = useContext(AppContext);
+	const [data, setData] = useState({ identifier: "", password: "" });
+	const handleLogin = () => {};
+	return (
+		<Container>
+			<Row>
+				<Col>
+					<div className="paper">
+						<div className="header">
+							<h2>ログイン</h2>
+						</div>
+					</div>
+					<section className="wrapper">
+						<Form>
+							<fieldset>
+								<FormGroup>
+									<Label>メールアドレス：</Label>
+									<Input
+										type="email"
+										name="identifier"
+										style={{
+											height: 50,
+											fontSize: "1.2rem",
+										}}
+									/>
+								</FormGroup>
+								<FormGroup>
+									<Label>パスワード：</Label>
+									<Input
+										type="password"
+										name="password"
+										style={{
+											height: 50,
+											fontSize: "1.2rem",
+										}}
+									/>
+								</FormGroup>
+								<span>
+									<a href="">
+										<small>
+											パスワードをお忘れですか？
+										</small>
+									</a>
+								</span>
+								<Button
+									style={{ float: "right", width: 120 }}
+									color="primary"
+									onClick={() => {
+										handleLogin();
+									}}
+								>
+									ログイン
+								</Button>
+							</fieldset>
+						</Form>
+					</section>
+				</Col>
+			</Row>
+			<style jsx>
+				{`
+					.paper {
+						text-align: center;
+						margin-top: 50px;
+					}
+					.header {
+						width: 100%;
+						margin-bottom: 30px;
+					}
+					.wrapper {
+						padding: 10px 30px 20px 30px;
+					}
+				`}
+			</style>
+		</Container>
+	);
+};
+
+export default Login;
