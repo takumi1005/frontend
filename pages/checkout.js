4b8cc37dc94c270c7e1632d0c8a52a7aa9fbdb07
takumi <takumim1005@gmail.com>
2022-12-30 10:59:18 +0900

    カード情報を打ち込むCardSectionコンポーネント作成

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,13 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { Col, Row } from "reactstrap";
 import Cart from "../components/Cart/index";
 import CheckOutForm from "../components/Checkout/CheckOutForm";
 
 const checkout = () => {
+	const stripePromise = loadStripe(
+		"pk_test_51MKWXiJcG3Cmdr5LhlhljvXthDI3YZVz3sWHOWa5WL6kPKdHcKIizd87fibIZ8TueFrT1seuOYpDnZnbMlrdbcCT00zLTlnRET"
+	);
 	return (
 		<Row>
 			<Col
@@ -15,7 +20,9 @@ const checkout = () => {
 				<Cart />
 			</Col>
 			<Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
-				<CheckOutForm />
+				<Elements stripe={stripePromise}>
+					<CheckOutForm />
+				</Elements>
 			</Col>
 		</Row>
 	);
